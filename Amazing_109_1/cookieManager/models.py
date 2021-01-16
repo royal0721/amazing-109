5fe6f879e57bca9deabcae59904f23a81aa14887
royal0721 <royal0721@gmail.com>
2021-01-17 02:02:54 +0800

    my amazing app14

--- Amazing_109_1/cookieManager/models.py
+++ Amazing_109_1/cookieManager/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Product(models.Model):  
     ProductID =  models.IntegerField()
     ProductName = models.TextField()  
-    ProductPrice = models.DecimalField(max_digits=19, decimal_places=2)
+    ProductPrice = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
     InventoryAmount = models.IntegerField()
 
 
@@ -15,16 +15,16 @@ class Customer(models.Model):
     CustomerID = models.IntegerField()
     CustomerName = models.TextField()  
     Gender = models.TextField()
-    pos_po = models.DecimalField(max_digits=19, decimal_places=2)
+    pos_po = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
     CLV = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
 
 class MarketingIndex(models.Model):  
     CustomerID = models.IntegerField()
-    ForecastPurchaseProb = models.DecimalField(max_digits=19, decimal_places=2)
-    CLV = models.DecimalField(max_digits=19, decimal_places=2)
-    WalletShare = models.DecimalField(max_digits=19, decimal_places=2)
-    CategoryDemandShare = models.DecimalField(max_digits=19, decimal_places=2)
-    ProportionofPositiveComment = models.DecimalField(max_digits=19, decimal_places=2)
+    ForecastPurchaseProb = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
+    CLV = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
+    WalletShare = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
+    CategoryDemandShare = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
+    ProportionofPositiveComment = models.DecimalField(max_digits=19, decimal_places=2, default=0.0)
 
 class CustomerPurchaseInfo(models.Model):  
     CustomerID = models.IntegerField()
