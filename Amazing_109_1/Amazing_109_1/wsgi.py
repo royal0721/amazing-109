7f628f5b04f9dfb745893eeee96ccee0001994ef
royal0721 <royal0721@gmail.com>
2021-01-17 00:42:54 +0800

    my amazing app3

--- Amazing_109_1/Amazing_109_1/wsgi.py
+++ Amazing_109_1/Amazing_109_1/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for Amazing_109_1 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Amazing_109_1.settings')
+from dj_static import Cling
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Amazing_109_1.settings")
+
+application = Cling(get_wsgi_application())
