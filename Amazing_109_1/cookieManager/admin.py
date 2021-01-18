345a6caf29151350887ce1cfcd9a9a3e9afccc3b
royal0721 <royal0721@gmail.com>
2021-01-18 14:39:57 +0800

    my amazing ta

--- Amazing_109_1/cookieManager/admin.py
+++ Amazing_109_1/cookieManager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product,Customer,MarketingIndex,CustomerPurchaseInfo
+
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(MarketingIndex)
+admin.site.register(CustomerPurchaseInfo)
